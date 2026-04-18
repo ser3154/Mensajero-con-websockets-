e40b5c8fbb3fca8d53105d9cccfe5018320b4501
ser3154 <servandocj45@outlook.es>
2026-04-18 12:48:57 -0700

    Websocket Servidor

---
+++ src/main/java/com/itson/WebsocketServer/controller/MensajeController.java
@@ -0,0 +1,15 @@
+package com.itson.WebsocketServer.controller;
+
+import com.itson.WebsocketServer.model.Mensaje;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MensajeController {
+    @MessageMapping("/envio")
+    @SendTo("/tema/mensajes")
+    public Mensaje envio(Mensaje mensaje){
+        return new Mensaje(mensaje.nombre(), mensaje.contenido());
+    }
+}
